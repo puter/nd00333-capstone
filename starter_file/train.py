4d18bd6ae9452ff084338ab4cd71413e8a33a7ba
Chadwick Puterbaugh <puterbaugh.cw@gmail.com>
2022-06-14 00:29:18 -0400

    Automl full file working

--- starter_file/train.py
+++ starter_file/train.py
@@ -10,15 +10,18 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-
+#from azureml.core.workspace import Workspace
+#ws = Workspace.from_config()
 
 
 def clean_data(data):
     # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    x_df["gender"] = x_df.marital.apply(lambda s: 1 if s == "Male" else 0)
-    x_df["ever_married"] = x_df.default.apply(lambda s: 1 if s == "Yes" else 0)
-    x_df["Residence_type"] = x_df.housing.apply(lambda s: 1 if s == "Urban" else 0)
+    x_df = data.to_pandas_dataframe().replace('N/A', np.nan).dropna()
+    x_df.drop('id', inplace=True)
+    x_df['bmi'] = x_df['bmi'].astype(float)
+    x_df["gender"] = x_df.gender.apply(lambda s: 1 if s == "Male" else 0)
+    x_df["ever_married"] = x_df.ever_married.apply(lambda s: 1 if s == "Yes" else 0)
+    x_df["Residence_type"] = x_df.Residence_type.apply(lambda s: 1 if s == "Urban" else 0)
     work_type = pd.get_dummies(x_df.work_type, prefix="work_type")
     x_df.drop("work_type", inplace=True, axis=1)
     x_df = x_df.join(work_type)
@@ -50,14 +53,18 @@ def main():
     args = parser.parse_args()
 
     run.log("Regularization Strength:", float(args.C))
+    print("Regularization Strength:", float(args.C))
     run.log("Max iterations:", int(args.max_iter))
+    print("Max iterations:", int(args.max_iter))
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
     joblib.dump(value=model, filename='outputs/model.joblib')
     accuracy = model.score(x_test, y_test)
     y_pred = model.predict(x_test)
     run.log("f1_score", f1_score(y_test, y_pred))
+    print("f1_score", f1_score(y_test, y_pred))
     run.log("Accuracy", float(accuracy))
+    print("Accuracy", float(accuracy))
     os.makedirs('outputs', exist_ok=True)
 
 if __name__ == '__main__':
